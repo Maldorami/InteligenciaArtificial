8feac149a4581e3db0311b1d9edd475800377b77
Alejandro Abecasis <ale.bros@hotmail.com>
2017-10-12 21:52:38 -0300

    Escenas: Flocking Scripts: Boid //TODO: calculate func Flocking //TODO: set neighbors to boids

---
+++ MaldoPathFinding/Assets/Flocking/Scripts/Boid.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public float speed = 5;
+    public float steeringSpeed = 3;
+    public List<Boid> neighbors;
+
+    bool move = false;
+    Vector3 target = new Vector2();
+
+    private void Start()
+    {
+        neighbors = new List<Boid>();
+    }
+
+    void Update()
+    {
+        if (move)
+        {
+            Vector3 diff = target - transform.position;
+            diff.Normalize();
+            float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+            Quaternion rot = Quaternion.AngleAxis(rot_z, Vector3.forward);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rot, steeringSpeed * Time.deltaTime);
+            transform.position += transform.right * speed * Time.deltaTime;
+
+            if (Vector2.Distance(transform.position, target) <= 0.5) move = false;
+        }
+    }
+
+    public void GoToPosition(Vector3 destPos)
+    {
+        move = true;
+        target = destPos;
+        Debug.Log("<color=yellow>" + destPos + "</color>");
+    }
+
+    public void AddNeighbors(Boid neighbor)
+    {
+        neighbors.Add(neighbor);
+    }
+
+    public void Calculate()
+    {
+
+    }
+
+    public void ClearNeighbor()
+    {
+        neighbors.Clear();
+    }
+
+}
---
+++ MaldoPathFinding/Assets/Flocking/Scripts/FlockingManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockingManager : MonoBehaviour
+{
+
+    List<Boid> _boids;
+
+    void Start()
+    {
+        GameObject[] tmp = GameObject.FindGameObjectsWithTag("Boid");
+        _boids = new List<Boid>();
+        for (int i = 0; i < tmp.Length; i++)
+        {
+            _boids.Add(tmp[i].GetComponent<Boid>());
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray rayHit = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            for (int i = 0; i < _boids.Count; i++)
+            {
+                _boids[i].GoToPosition(rayHit.origin);
+            }
+
+        }
+    }
+}
